a961df6ef79e82d996f86a4b183e5eb6d2282b17
Yun ShinYeong <freeswsy@gmail.com>
2018-05-21 20:57:36 +0900

    FileComparator
    FileComparator

--- SimpleMerge/src/MergeController.java
+++ SimpleMerge/src/MergeController.java
@@ -1,9 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
 
+/*패널간의 메소드를 관리하는 컨트롤러(interpanel controller),
+ * compare, traverse, merge 메소드 포함 */
 public class MergeController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private PanelInfo leftPanel;
+	private PanelInfo rightPanel;
+	private ArrayList<String> leftFileController;
+	private ArrayList<String> rightFileController;
+	private FileComparator fc;
+	private int[] diffIndex;
+	
+	MergeController(){
+		System.out.println("No input panels");
 	}
-
+	
+	MergeController(PanelInfo leftPanel, PanelInfo rightPanel) {
+		this.leftPanel = leftPanel;
+		this.rightPanel = rightPanel;
+		/* left, right Panel의 속성에서 String을 받아와서
+		 * ArrayList로 만들어야함
+		 * 그리고 diffIndex를 생성
+		 * FileComparator 생성
+		 */
+	}
+	
+	int[] getDiffIndex() {
+		return this.diffIndex;
+	}
+	
+	int traversePrevious() {
+		
+		int index = -1;
+		// TODO
+		
+		return index;
+	}
+	
+	int traverseNext() {
+		
+		int index = -1;
+		// TODO
+		
+		return index;
+	}
+	
+	void copyToLeft() {
+		// TODO
+	}
+	
+	void copyToRight() {
+		// TODO
+	}	
 }

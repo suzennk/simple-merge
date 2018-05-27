12f19623827b04ecd5a8e9216431067ed6e47544
Yun ShinYeong <freeswsy@gmail.com>
2018-05-27 23:40:52 +0900

    test printDiff
    test printDiff methods to identify differences between two strings

--- SimpleMerge/src/FileComparator.java
+++ SimpleMerge/src/FileComparator.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class FileComparator {
 	private int[][] C;
 	
-	/** <sequence alignment>
+	/** Computing the length of the LCS
 	 * 길이가 m인 text1과 길이가 n인 text2를 입력받아 모든 1 <= i <= m 와 1 <= j <= n에 대해
 	 * text1[1..i] 와 text2[1..j] 사이의 값에 대해 LCS를 연산하고,
 	 * C[i, j]에 저장한다. C[m, n]은 text1과 text2에 대한 LCS 값을 가지게 된다.
@@ -16,12 +16,12 @@ public class FileComparator {
 		/* 행이나 열의 index가 0인 element는 0으로 초기화한다 */
 		for (int i = 0; i <= m; i++)
 			C[i][0] = 0;
-		for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= n; j++)
 			C[0][j] = 0;
 	
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (text1.get(i).equals(text2.get(j)))
+				if (text1.get(i - 1).equals(text2.get(j - 1)))
 					C[i][j] = C[i-1][j-1] + 1;
 				else
 					C[i][j] = (C[i][j-1] > C[i-1][j])? C[i][j-1] : C[i-1][j];
@@ -32,7 +32,32 @@ public class FileComparator {
 	}
 	
 	/**
-	 *	backtracking을 이용하여 C의 표를 연산한다. 
+	 * print diff
+	 */
+	public void printDiff(int[][] C, ArrayList<String> text1, ArrayList<String> text2, int i, int j) {
+		if (i > 0 && j > 0 && text1.get(i - 1).equals(text2.get(j-1))) {
+			printDiff(C, text1, text2, i-1, j-1);
+			System.out.println(i-1+":  " + text1.get(i-1));
+		}
+		else if (j > 0 && (i == 0 || C[i][j-1] >= C[i-1][j])) {
+			printDiff(C, text1, text2, i, j - 1);
+			System.out.println(i-1+":+ "+ text2.get(j-1));
+		}
+		else if (i > 0 && (j == 0 || C[i][j-1] < C[i-1][j])) {
+			printDiff(C, text1, text2, i-1, j);
+			System.out.println(i-1+":- " + text1.get(i-1));
+		}
+		else
+			System.out.println("");
+	}
+	
+	
+	
+	/**
+	 * Reading out a LCS
+	 *	backtracking을 이용하여 C의 배열을 연산한다. 
+	 *	접두사의 마지막 문자들이 같으면, 원래의 LCS에 그 문자를 더한 String을 return
+	 *	접두사의 마지막 문자들이 다르면, 가장 큰 LCS를 return
 	 */
 	public String backtrack(int[][] C, ArrayList<String> text1, ArrayList<String> text2, int i, int j) {
 		if (i == 0 || j == 0)
@@ -47,5 +72,55 @@ public class FileComparator {
 		}
 	}
 	
+	/**
+	 * Reading out all LCSs
+	 * 	text1과 text2가 같은 길이의 결과를 return 한다면,
+	 *	결과로 나오는 모든 subsequences를 읽어들인다
+	 * 
+	 */
+	public String backtrackAll(int[][] C, ArrayList<String> text1, ArrayList<String> text2, int i , int j) {
+		// TODO
+		return "";
+	}
 	
+	/**
+	 * @return C
+	 */
+	public int[][] getC() {
+		return this.C;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		FileComparator fc = new FileComparator();
+		ArrayList<String> s1 = new ArrayList<String>();
+		ArrayList<String> s2 = new ArrayList<String>();
+		s1.add("abcdef");
+		s2.add("abcdef");
+		s1.add("aaa");
+		s2.add("bbb");
+		s1.add("hihihi");
+		s2.add("hihihi");
+		
+		fc.LCSLength(s1, s2);
+		fc.printDiff(fc.getC(), s1, s2, s1.size(), s2.size());
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

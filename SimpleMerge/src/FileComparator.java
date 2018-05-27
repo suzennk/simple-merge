b788cdc0092af8fa99926141c15eecd227d2061f
Yun ShinYeong <freeswsy@gmail.com>
2018-05-28 01:49:42 +0900

    Implemented traverse, merge method
    Implemented traverse and merge method in MergeController class and modified FileComparator class

--- SimpleMerge/src/FileComparator.java
+++ SimpleMerge/src/FileComparator.java
@@ -11,18 +11,21 @@ public class FileComparator {
 	 * 같은 내용을 가진 줄은 서로의 index를 저장하고
 	 * 다른 내용을 가진 줄에는 -1을 저장한다.
 	 * ex)
-	 * <Left>	<diffLeft>	<Right>	<diffRight>
-	 * abc		0			abc		0
-	 * de		-1			fgh		2
+	 * <Left>	<leftDiffIndex>	<Right>	<rightDiffIndex>
+	 * abc		0				abc		0
+	 * de		-1				fgh		2
 	 * fgh		1
 	 */
-	private ArrayList<Integer> diffLeft;
-	private ArrayList<Integer> diffRight;
+	private ArrayList<Integer> leftDiffIndex;
+	private ArrayList<Integer> rightDiffIndex;
 	private final Integer diff = -1;
 
-	public FileComparator() {
-		diffLeft = new ArrayList<Integer>();
-		diffRight = new ArrayList<Integer>();
+	public FileComparator(ArrayList<String> left, ArrayList<String> right) {
+		leftDiffIndex = new ArrayList<Integer>();
+		rightDiffIndex = new ArrayList<Integer>();
+		
+		this.LCSLength(left, right);
+		this.compare(this.C, left, right, left.size(), right.size());
 	}
 
 	/**
@@ -30,9 +33,9 @@ public class FileComparator {
 	 * n인 text2를 입력받아 모든 1 <= i <= m 와 1 <= j <= n에 대해 text1[1..i] 와 text2[1..j] 사이의
 	 * 값에 대해 LCS를 연산하고, C[i, j]에 저장한다. C[m, n]은 text1과 text2에 대한 LCS 값을 가지게 된다.
 	 */
-	public int LCSLength(ArrayList<String> text1, ArrayList<String> text2) {
-		int m = text1.size();
-		int n = text2.size();
+	public int LCSLength(ArrayList<String> left, ArrayList<String> right) {
+		int m = left.size();
+		int n = right.size();
 		C = new int[m + 1][n + 1];
 
 		/* 행이나 열의 index가 0인 element는 0으로 초기화한다 */
@@ -43,7 +46,7 @@ public class FileComparator {
 
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (text1.get(i - 1).equals(text2.get(j - 1)))
+				if (left.get(i - 1).equals(right.get(j - 1)))
 					C[i][j] = C[i - 1][j - 1] + 1;
 				else
 					C[i][j] = (C[i][j - 1] > C[i - 1][j]) ? C[i][j - 1] : C[i - 1][j];
@@ -53,46 +56,28 @@ public class FileComparator {
 	}
 
 	/**
-	 * print diff
-	 * ******************이 메소드 삭제해도됨 테스트하려고 만든거******************
-	 */
-	public void printDiff(int[][] C, ArrayList<String> text1, ArrayList<String> text2, int i, int j) {
-		if (i > 0 && j > 0 && text1.get(i - 1).equals(text2.get(j - 1))) {
-			printDiff(C, text1, text2, i - 1, j - 1);
-			System.out.println(i - 1 + ":  " + text1.get(i - 1));
-		} else if (j > 0 && (i == 0 || C[i][j - 1] >= C[i - 1][j])) {
-			printDiff(C, text1, text2, i, j - 1);
-			System.out.println(i - 1 + ":+ " + text2.get(j - 1));
-		} else if (i > 0 && (j == 0 || C[i][j - 1] < C[i - 1][j])) {
-			printDiff(C, text1, text2, i - 1, j);
-			System.out.println(i - 1 + ":- " + text1.get(i - 1));
-		} else
-			System.out.println("");
-	}
-
-	/**
 	 * @param C: matrix of LCS length between text 1 and text2
-	 * @param text1
-	 * @param text2
+	 * @param left
+	 * @param right
 	 * @param i: length(size) of text1
 	 * @param j: length(size) of text2
 	 */
-	public void compare(int[][] C, ArrayList<String> text1, ArrayList<String> text2, int i, int j) {
+	public void compare(int[][] C, ArrayList<String> left, ArrayList<String> right, int i, int j) {
 		/* if two strings have same line, store mutual index */
-		if (i > 0 && j > 0 && text1.get(i - 1).equals(text2.get(j - 1))) {
-			compare(C, text1, text2, i - 1, j - 1);
-			this.diffLeft.add(i - 1, j - 1);
-			this.diffRight.add(j - 1, i - 1);
+		if (i > 0 && j > 0 && left.get(i - 1).equals(right.get(j - 1))) {
+			compare(C, left, right, i - 1, j - 1);
+			this.leftDiffIndex.add(i - 1, j - 1);
+			this.rightDiffIndex.add(j - 1, i - 1);
 		}
 		
 		/* if two strings have different line, store diff(-1) */
 		else if (j > 0 && (i == 0 || C[i][j - 1] >= C[i - 1][j])) {
-			compare(C, text1, text2, i, j - 1);
-			this.diffRight.add(j - 1, diff);
+			compare(C, left, right, i, j - 1);
+			this.rightDiffIndex.add(j - 1, diff);
 		} 
 		else if (i > 0 && (j == 0 || C[i][j - 1] < C[i - 1][j])) {
-			compare(C, text1, text2, i - 1, j);
-			this.diffLeft.add(i - 1, diff);
+			compare(C, left, right, i - 1, j);
+			this.leftDiffIndex.add(i - 1, diff);
 		}
 	}
 
@@ -107,21 +92,20 @@ public class FileComparator {
 	 * @return diffLeft
 	 */
 	public ArrayList<Integer> getDiffLeft(){
-		return this.diffLeft;
+		return this.leftDiffIndex;
 	}
 	
 	/**
 	 * @return diffRight
 	 */
 	public ArrayList<Integer> getDIffRight(){
-		return this.diffRight;
+		return this.rightDiffIndex;
 	}
 
 	public static void main(String[] args) {
 		/* This main is a test for this class.
 		 * You can delete this if you don't need.
 		 * */
-		FileComparator fc = new FileComparator();
 		ArrayList<String> s1 = new ArrayList<String>();
 		ArrayList<String> s2 = new ArrayList<String>();
 		s1.add("abcdef");
@@ -135,17 +119,18 @@ public class FileComparator {
 		s2.add("same part");
 		s2.add("hihihi");
 
+		FileComparator fc = new FileComparator(s1, s2);
 		fc.LCSLength(s1, s2);
 		fc.compare(fc.getC(), s1, s2, s1.size(), s2.size());
 
 		System.out.println("Left Panel=========");
 		for (int i = 0; i < s1.size(); i++) {
-			System.out.println("["+fc.diffLeft.get(i)+"]\t"+s1.get(i));
+			System.out.println("["+fc.leftDiffIndex.get(i)+"]\t"+s1.get(i));
 		}
 
 		System.out.println("\nRight Panel=========");
 		for (int i = 0; i < s2.size(); i++) {
-			System.out.println("["+fc.diffRight.get(i)+"]\t"+s2.get(i));
+			System.out.println("["+fc.rightDiffIndex.get(i)+"]\t"+s2.get(i));
 		}
 
 	}

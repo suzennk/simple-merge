614d5e47d310223257d086193f7b3d2282676582
engyengKim <engyeng9797@gmail.com>
2018-05-31 23:34:02 +0900

    modify PV: solve issues #35,#36

--- SimpleMerge/src/PanelView.java
+++ SimpleMerge/src/PanelView.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
 
@@ -32,6 +34,9 @@ public class PanelView extends JPanel {
 
 	
 	private Color panelColor;
+	
+	private ArrayList<Integer> diffIndex;
+	private int[] block; // block to be colored: {begin_idx, end_idx}
 	   
 	
 	public PanelView() throws Exception{
@@ -41,7 +46,10 @@ public class PanelView extends JPanel {
 		editorPanel 	= new JPanel();
 		titlePanel 		= new JPanel();
 		
-		panelColor 		= new Color(0,0,0); //set color default as WHITE 
+		panelColor 		= new Color(0,0,0); // set color default as WHITE 
+		diffIndex		= new ArrayList<Integer>(); // highlighted when compare & traverse
+		block 			= new int[] {0, 0}; // default value : nothing to be colored
+
 		
 		// set image icon
 		load_icon 		= new ImageIcon("res/load.png");
@@ -116,6 +124,7 @@ public class PanelView extends JPanel {
 		this.setVisible(true);
 		
 		this.setMode(Mode.VIEW);
+		editBtn.setEnabled(false); // at the start, only loadBtn enable!
 		
 		// keyboard input
 		textArea.addKeyListener(new KeyAdapter() {
@@ -239,7 +248,8 @@ public class PanelView extends JPanel {
 		pc.setFileContent(editedContent);
 		
   		if (pc.save()) {
-  			setMode(Mode.VIEW);
+  			if(pc.getMode()!=Mode.COMPARE)
+  				setMode(Mode.VIEW);
   		}
 	}
 	
@@ -253,7 +263,8 @@ public class PanelView extends JPanel {
 		if (fd.getFile() != null) {
 			String filePath = fd.getDirectory() + fd.getFile();
 			if (pc.saveAs(filePath)) {
-				setMode(Mode.VIEW);	
+				if(pc.getMode()!=Mode.COMPARE)
+					setMode(Mode.VIEW);	
 			}
 		}
 	}
@@ -266,4 +277,11 @@ public class PanelView extends JPanel {
 		panelColor=new Color(x,y,z);
 	}
 	
+	public void setDiffIndex(ArrayList<Integer> diffIndex){
+		this.diffIndex=diffIndex;
+	}
+	
+	public void setBlock(int[] block){
+		this.block=block;
+	}
 }

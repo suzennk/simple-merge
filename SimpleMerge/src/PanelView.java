9ecc93b6d88c2f86850eeb125504b0b43928be97
Sujin Kim <gfsusan@naver.com>
2018-06-06 13:32:59 +0900

    Disable edit button when no file is open

--- SimpleMerge/src/PanelView.java
+++ SimpleMerge/src/PanelView.java
@@ -303,6 +303,7 @@ public class PanelView extends JPanel {
 	
 		if (!tec.fileIsOpen()) {
 			textArea.setText("Click the Load Button.");
+			editBtn.setEnabled(false);
 		}
 
 		if (tec.isUpdated()) {

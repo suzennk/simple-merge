30c6f89e4eea47ed9af6f8332c5359f6e9d91f3f
Sujin Kim <gfsusan@naver.com>
2018-06-08 08:15:45 +0900

    Load test

--- SimpleMerge/src/test/TextEditorTests.java
+++ SimpleMerge/src/test/TextEditorTests.java
@@ -5,6 +5,14 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,13 +28,39 @@ import simplemerge.TextEditorModel;
 class TextEditorTests {
 
 	TextEditorModel tem;
+	static String inputFile;
+	static String content;
+	static String inputFile_no;
 	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		inputFile = new String("input.txt");
+		inputFile_no = new String("input_no.txt");
 		
+		// Generate a valid input file
+		File input = new File(inputFile);
+		FileWriter fw= new FileWriter(inputFile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		content = new String();
+		int i = 0;
+		content += String.valueOf(i++);
+		while (i++ < 100) {
+			content += "\r\n";
+			content += String.valueOf(i);
+		}
+		bw.write(content);
+		
+		if (bw != null)
+			bw.close();
+		if (fw != null)
+			fw.close();
+		
+		// Generate an invalid input file by creating a file, and then deleting it.
+		File input_no = new File(inputFile_no);
+		input_no.delete();
 	}
 
 	/**
@@ -53,9 +87,22 @@ class TextEditorTests {
 	}
 
 	@Test
-	void loadTest() {
-		tem.load("test.txt");
-		fail("Not yet implemented");
+	void loadTest() throws IOException {
+		
+		assertEquals(true, tem.load("TestInputs/input.txt"));
+		assertEquals(false, tem.isUpdated());
+		assertEquals(readFile("TestInputs/input.txt"), tem.getFileContentBuffer());
+		assertEquals(tem.getFileContentBuffer(), tem.getFileContentBuffer());
+		
+		assertEquals(true, tem.load("TestInputs/number.txt"));
+		assertEquals(false, tem.isUpdated());
+		assertEquals(readFile("TestInputs/number.txt"), tem.getFileContentBuffer());
+		assertEquals(tem.getFileContentBuffer(), tem.getFileContentBuffer());
+		
+		assertEquals(true, tem.load("TestInputs/test.txt"));
+		assertEquals(false, tem.isUpdated());
+		assertEquals(readFile("TestInputs/test.txt"), tem.getFileContentBuffer());
+		assertEquals(tem.getFileContentBuffer(), tem.getFileContentBuffer());
 	}
 	
 	@Test
@@ -68,4 +115,20 @@ class TextEditorTests {
 		
 	}
 	
+	String readFile(String filepath) throws IOException {
+		File f = new File(filepath);
+		FileReader fr = new FileReader(filepath);
+		BufferedReader br = new BufferedReader(fr);
+
+		String originalFileContent = new String();
+		String s = br.readLine();
+
+		if (s != null)
+			originalFileContent += s;
+		while ((s = br.readLine()) != null) {
+			originalFileContent += "\r\n";
+			originalFileContent += s;
+		}
+		return originalFileContent;
+	}
 }

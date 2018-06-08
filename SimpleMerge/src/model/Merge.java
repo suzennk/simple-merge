7beb9906d9169a2916470a5cec5776c58bf284b4
anvel913 <eddysonkr@gmail.com>
2018-06-08 21:53:52 +0900

    Update Merge.java

--- SimpleMerge/src/model/Merge.java
+++ SimpleMerge/src/model/Merge.java
@@ -17,7 +17,7 @@ public class Merge {
 	private ArrayList<String> leftViewContents;
 	private ArrayList<String> rightViewContents;
 	/**
-	 * arraylist which stores difference indices of each panel
+	 * arrayList which stores difference indices of each panel
 	 * the index is started at 1, not 0
 	 */
 	private ArrayList<Integer> leftDiffIndex;
@@ -184,102 +184,5 @@ public class Merge {
 		}
 	}
 
-	/**
-	 * Used for the test of main class of this class. You can delete this method. It
-	 * prints the index which includes blanks.
-	 */
-	void printArranged() {
-		System.out.println("Left Panel=========");
-		for (int i = 1; i < this.leftDiffIndex.size(); i++)
-			System.out.println("[" + i + "] " + this.leftDiffIndex.get(i));
-
-		System.out.println("\nRight Panel=========");
-		for (int i = 1; i < this.rightDiffIndex.size(); i++)
-			System.out.println("[" + i + "] " + this.rightDiffIndex.get(i));
-	}
-
-	//
-	/**
-	 * Used for the test of main class of this class. You can delete this method. It
-	 * prints the index which does not include blanks.
-	 */
-	void printAll() {
-		System.out.println("Left Panel=========");
-		for (int i = 1; i < this.leftFileSourceContents.size(); i++)
-			System.out.println(this.leftFileSourceContents.get(i));
-
-		System.out.println("\nRight Panel=========");
-		for (int i = 1; i < this.rightFileSourceContents.size(); i++)
-			System.out.println(this.rightFileSourceContents.get(i));
-	}
-
-	public static void main(String[] args) {
-		/*
-		 * This main is a test for this class. You can delete this if you don't need.
-		 */
-		Scanner s = new Scanner(System.in);
-		int[] index = new int[2];
-		TextEditorModel left = new TextEditorModel();
-		TextEditorModel right = new TextEditorModel();
-
-		left.setFileContentBuffer("\r\n" + "\r\n" + "\r\n" + "AAA\r\n" + "\r\n" + "\r\n" + "");
-
-		right.setFileContentBuffer("\r\n" + "\r\n" + "\r\n" + "AA\r\n" + "1\r\n" + "\r\n" + "");
-
-		Merge mc = new Merge(left, right);
-
-		boolean iterate = true;
-		while (iterate) {
-			System.out.println("\n1. print");
-			System.out.println("2. print arranged");
-			System.out.println("3. traverse previous");
-			System.out.println("4. traverse next");
-			System.out.println("5. copy to left");
-			System.out.println("6. copy to right");
-			System.out.println("7. exit");
-			System.out.print("Select menu: ");
-			int menu = s.nextInt();
-			switch (menu) {
-
-			case 1:
-				mc.printAll();
-				break;
-			case 2:
-				mc.printArranged();
-				break;
-			case 3:
-				mc.traversePrevious();
-				if (index[0] != -1 && index[1] != -1) {
-					System.out.println("Traverse line " + index[0] + " to " + index[1]);
-					for (int i = index[0]; i <= index[1]; i++)
-						System.out.println(mc.leftFileSourceContents.get(i));
-				}
-				break;
-			case 4:
-				mc.traverseNext();
-				if (index[0] != -1 && index[1] != -1) {
-					System.out.println("Traverse line " + index[0] + " to " + index[1]);
-					for (int i = index[0]; i <= index[1]; i++)
-						System.out.println(mc.leftFileSourceContents.get(i));
-				}
-				break;
-			case 5:
-				mc.copyToLeft();
-				break;
-			case 6:
-				mc.copyToRight();
-				break;
-			case 7:
-				iterate = false;
-				break;
-			default:
-				System.out.println("wrong choice");
-				continue;
-			}
-
-		}
-		System.out.println("Exit Program.");
-		s.close();
-	}
 
 }
